b313f1a070d562bcc03e6123c236b75f4713c34e
aSydiK <mickael.medel@gmail.com>
2010-07-08 13:36:31 -0400

    .

--- pointstream.js
+++ pointstream.js
@@ -24,7 +24,7 @@ function PointStream(){
   var VBOs;
   
   // browser detection to handle differences such as mouse scrolling
-  var browser     = -1 ;
+  var browser     = -1;
   const MINEFIELD = 0;
   const CHROME    = 1;
   const CHROMIUM  = 2;
@@ -341,6 +341,91 @@ function PointStream(){
 
   /**
   */
+  function keyCodeMap(code, shift) {
+    // Letters
+    if (code >= 65 && code <= 90) { // A-Z
+      // Keys return ASCII for upcased letters.
+      // Convert to downcase if shiftKey is not pressed.
+      if (shift) {
+        return code;
+      }
+      else {
+        return code + 32;
+      }
+    }
+    // Numbers and their shift-symbols
+    else if (code >= 48 && code <= 57) { // 0-9
+      if (shift) {
+        switch (code) {
+        case 49:
+          return 33; // !
+        case 50:
+          return 64; // @
+        case 51:
+          return 35; // #
+        case 52:
+          return 36; // $
+        case 53:
+          return 37; // %
+        case 54:
+          return 94; // ^
+        case 55:
+          return 38; // &
+        case 56:
+          return 42; // *
+        case 57:
+          return 40; // (
+        case 48:
+          return 41; // )
+        }
+      }
+    }
+    // Coded keys
+    else if (codedKeys.indexOf(code) >= 0) { // SHIFT, CONTROL, ALT, LEFT, RIGHT, UP, DOWN
+      p.keyCode = code;
+      return p.CODED;
+    }
+    // Symbols and their shift-symbols
+    else {
+      if (shift) {
+        switch (code) {
+        case 107:
+          return 43; // +
+        case 219:
+          return 123; // {
+        case 221:
+          return 125; // }
+        case 222:
+          return 34; // "
+        }
+      } else {
+        switch (code) {
+        case 188:
+          return 44; // ,
+        case 109:
+          return 45; // -
+        case 190:
+          return 46; // .
+        case 191:
+          return 47; // /
+        case 192:
+          return 96; // ~
+        case 219:
+          return 91; // [
+        case 220:
+          return 92; // \
+        case 221:
+          return 93; // ]
+        case 222:
+          return 39; // '
+        }
+      }
+    }
+    return code;
+  };
+
+  /**
+  */
   var xb = {
 
     /**
@@ -554,19 +639,20 @@ function PointStream(){
       }
     },
     
-    _keyDown: function(){
+    _keyDown: function(evt){
+      key = keyCodeMap(evt.keyCode, evt.shiftKey)
       if(typeof xb.keyDown === "function"){
         xb.keyDown();
       }
     },
     
-    _keyPressed: function(){
+    _keyPressed: function(evt){
       if(typeof xb.keyPressed === "function"){
         xb.keyPressed();
       }
     },
     
-    _keyUp: function(){
+    _keyUp: function(evt){
       if(typeof xb.keyUp === "function"){
         xb.keyUp();
       }
