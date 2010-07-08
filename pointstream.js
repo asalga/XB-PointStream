ddeccb3c5be5309ee110f5fbaaa78115f44d9488
aSydiK <asydik@india.proximity.on.ca>
2010-07-08 17:08:14 -0400

    improved code provided by notmasteryet

--- pointstream.js
+++ pointstream.js
@@ -1,12 +1,11 @@
-var ps_include = function(path) {
-  var lastScript = document.getElementsByTagName("head")[0].lastChild;
-  var fullUrl = lastScript.src.substring(0, lastScript.src.lastIndexOf('/') + 1) + path; 
-  document.write('<' + 'script');
-  document.write(' language="javascript"');
-  document.write(' type="text/javascript"');
-  document.write(' src="' + fullUrl + '">');
-  document.write('</' + 'script' + '>');
-}
+var ps_include = ((function(lastScript) { 
+  return (function(path) {
+    var fullUrl = lastScript.src.substring(0, lastScript.src.lastIndexOf('/') + 1) + path; 
+    var newScript = document.createElement("script"); 
+    newScript.src = fullUrl; 
+    document.getElementsByTagName("head")[0].appendChild(newScript);
+  });
+})(document.getElementsByTagName("head")[0].lastChild));
 
 ps_include('mjs.js');
 ps_include('psapi.js');
