087a73f9a8aaca29cfb110f930182584e69d1c7f
Andor <andor@MBP-55.local>
2011-02-16 15:42:05 -0500

    changed gendocs to bash

--- tools/jsdoc-toolkit/gendocs.sh
+++ tools/jsdoc-toolkit/gendocs.sh
@@ -1,23 +1,14 @@
-#!/bin/tcsh
+#!/bin/bash
 
-# The following arguments are used when generating the documentation.
-#
-# -v (verbose) = Displays what the tool is doing on the console.
-#
-# -t (template) = The template used.
-#
-# -d (destination) = The directory where the generated files should be placed.
+xbps=$PWD
+jsd=$PWD
 
-set xbps=$PWD
-set jsd=$xbps
-
-# Instead of having to force users to set paths, we take
-# care if this script is called from make or directly by the user
-if($#argv == 1) then
-	set jsd=${xbps}/tools/jsdoc-toolkit
+# Make will pass in a dummy variable
+if [ $# -eq 1 ]
+then
+  jsd=$xbps/tools/jsdoc-toolkit
 else
-	set xbps=$xbps/../../
-endif
+  xbps=$xbps/../../
+fi
 
-#
 java -jar $jsd/jsrun.jar $jsd/app/run.js -a -v -t=$jsd/templates/jsdoc -d=$xbps/docs/ $xbps/psapi.js $xbps/parsers/asc.js
