6426655fc1d56d5d6049cf46339761c3c529bb4b
Andor <andor@MBP-55.local>
2011-02-16 18:00:42 -0500

    shortened parser code.

--- tests/user_parser/demo.js
+++ tests/user_parser/demo.js
@@ -1,10 +1,10 @@
 function start(){
-  var ps = new PointStream(); 
+  var ps = new PointStream();
   ps.setup(document.getElementById('canvas'));
   ps.registerParser("foo", FOO_Parser);
   ps.onRender = function render() {
     ps.translate(0, 0, -20);
     ps.render(acorn);
   };
-  var acorn = ps.load("../../clouds/acorn_only_verts.foo");
+  var acorn = ps.load("../../clouds/foo.foo");
 }
