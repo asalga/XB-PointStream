ccf2e18905e2ef257dacff1224899b33973da2b1
Andor <andor@MBP-55.local>
2011-03-02 16:48:13 -0500

    bit of cleanup

--- tools/sundae/resources/tests.js
+++ tools/sundae/resources/tests.js
@@ -7,16 +7,16 @@
         "dependancyURL": ["../../mjs.js",
                           "../../psapi.js",
                           "../../parsers/asc.js"],
-        "referenceImageURL": "resources/only_verts.png",
-        "run": {"src": "resources/only_verts.js", "func": "start" }
+        "referenceImageURL": "resources/only_verts/only_verts.png",
+        "run": {"src": "resources/only_verts/only_verts.js", "func": "start" }
       },
       {
         "name": "1",
         "dependancyURL": ["../../mjs.js",
                           "../../psapi.js",
                           "../../parsers/asc.js"],
-        "referenceImageURL": "resources/acorn_unlit.png",
-        "run": {"src": "resources/acorn_unlit.js", "func": "start" }
+        "referenceImageURL": "resources/acorn_unlit/acorn_unlit.png",
+        "run": {"src": "resources/acorn_unlit/acorn_unlit.js", "func": "start" }
       },
       {
         "name": "user shader",
