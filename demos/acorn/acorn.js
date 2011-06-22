f6eb6201d7cb28e7ca8ae62f0a0c96e0322e2e4e
Andor Salga <andor.salga@senecac.on.ca>
2011-06-22 14:15:34 -0400

    started fix

--- demos/acorn/acorn.js
+++ demos/acorn/acorn.js
@@ -1,7 +1,7 @@
 var ps, acorn;
 
 // Create an orbit camera halfway between the closest and farthest point
-var cam = new OrbitCam({closest:10, farthest:20, distance: 20});
+var cam = new OrbitCam({closest:10, farthest:100, distance: 40});
 var isDragging = false;
 var rotationStartCoords = [0, 0];
 
@@ -48,6 +48,7 @@ function keyDown(){
 }
 
 function render(){
+
   if(isDragging === true){		
 		// how much was the cursor moved compared to last time
 		// this function was called?
@@ -96,17 +97,43 @@ function render(){
 function start(){
   ps = new PointStream();
   document.getElementById('debug').innerHTML += "XB PointStream Version: " + ps.version;
-  
   ps.setup(document.getElementById('canvas'));
-  
-  ps.background([0, 0, 0, 0.5]);
-  ps.pointSize(5);
+  ps.background([1, 1, 1, 1.0]);
 
+  fixedFunctionProg = ps.createProgram(fixedFunctionVert, fixedFunctionFrag);
+  ps.useProgram(fixedFunctionProg);
+
+  ps.uniformf("lights0.on", 1);
+
+  ps.uniformi("matOn", 1);
+  ps.uniformf("matShininess", 100);
+  ps.uniformf("matAmbient", [0.3, 0.3, 0.3]); // 204/255
+  ps.uniformf("matDiffuse", [1, 1, 1]); // 51/255
+  ps.uniformf("matSpecular", [0.7, 1, 1]);
+  
+  ps.uniformf("lights0.ambient",  [0.5, 0.5, 0.5]);
+  ps.uniformf("lights0.diffuse",  [0.7, 0.7, 0.7]);
+  ps.uniformf("lights0.specular", [0.6, 0.6, 0.6]);
+  ps.uniformf("lights0.position", [0, 50, 50]);  
+  
+/*  ps.uniformi("matOn", 1);
+  ps.uniformf("matShininess", 18);
+  ps.uniformf("matAmbient", [0.8, 0.8, 0.8]); // 204/255
+  ps.uniformf("matDiffuse", [0.2, 0.2, 0.2]); // 51/255
+  ps.uniformf("matSpecular", [1, 1, 1]);
+  
+  ps.uniformf("lights0.ambient",  [0.3, 0.3, 0.3]);
+  ps.uniformf("lights0.diffuse",  [0.7, 0.7, 0.7]);
+  ps.uniformf("lights0.specular", [0.1, 0.1, 0.1]);
+  ps.uniformf("lights0.position", [0, 0, 10]);*/
+  
+  ps.pointSize(3.0);
+
+  acorn = ps.load("../../clouds/acorn.psi");
+  
   ps.onRender = render;
   ps.onMouseScroll = zoom;
   ps.onMousePressed = mousePressed;
   ps.onMouseReleased = mouseReleased;
   ps.onKeyDown = keyDown;
-  
-  acorn = ps.load("../../clouds/acorn.asc");
 }
