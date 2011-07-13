761ae82499f1f7a0d742e13998935382891e5d7f
Andor Salga <andor.salga@senecac.on.ca>
2011-07-13 16:32:10 -0400

    added ref tests, one failure. Need to file a separate bug for that.

---
+++ tools/sundae/resources/mask_70K/psi.js
@@ -0,0 +1,19 @@
+function start(cvs, cb){
+  var ps = new PointStream();
+  ps.setup(cvs);
+  ps.pointSize(1);
+  ps.onRender = function(){
+    if(pointCloud.status === 3){
+      ps.background([0.9, 0.5, 0.4, 1]);
+      ps.clear();
+      
+      var c = pointCloud.getCenter();
+      
+      ps.translate(-c[0], -c[1], -c[2]-55);
+      ps.render(pointCloud);
+      cb();
+      ps.onRender = function(){};
+    }
+  };
+  var pointCloud = ps.load('../../clouds/mask_70K.psi');
+}
