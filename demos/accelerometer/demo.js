ed189a128d9efd98d80fb5e3915beade9d67807b
Andor Salga <andor.salga@senecac.on.ca>
2011-08-25 10:06:25 -0400

    fixed sms support in Firefox, Minefield and Chromium.

--- demos/accelerometer/demo.js
+++ demos/accelerometer/demo.js
@@ -5,7 +5,6 @@ var ps, mickey;
 var x = 0;
 var y = 0;
 
-
 function pointLight(light){
   var lightName = "lights" + light.id;
   ps.uniformi( lightName + ".isOn", true);
@@ -33,18 +32,31 @@ function render() {
   ps.render(mickey);
 }
 
-function handleOrientation(data){
+function handleOrientation(eventData){
   // swap the values to make the orientation make sense.
   // if the device is pitched forward or backward, (moz y)
   // convert to a rotation about x.
-  
+
   // if the device is rolled left or right, (moz x)
   // convert to a rotation about y
-  x = data.y;
-  y = data.x;
+  if(eventData.gamma){
+    y = -eventData.gamma/70;
+    x = eventData.beta/70;
+  }
+  else if(eventData.x){
+  }
+  else if(eventData.accelerationIncludingGravity){
+    y = eventData.accelerationIncludingGravity.x;
+    x = eventData.accelerationIncludingGravity.y;
+  }
 }
 
 function start(){
+
+  if(!window.DeviceMotionEvent){
+    document.getElementById('msg').innerHTML = "Your browser does not support the motion event.";
+  }
+
   ps = new PointStream();
   ps.setup(document.getElementById('canvas'));
   ps.background([0.3, 0.5, 0.6, 0.6]);
@@ -57,7 +69,9 @@ function start(){
   
  pointLight({id:0, ambient:[.2,.2,.2], diffuse:[.7,.7,.7], attenuation:[1,0,0], position: [0,0,1]});
 
-  addEventListener("MozOrientation", handleOrientation, true);
+  addEventListener("devicemotion", handleOrientation, false);
+  addEventListener("deviceorientation", handleOrientation, false);
+  
   ps.onRender = render;
   
   mickey = ps.load("../../clouds/mickey.asc");
